d368bac4e63958114228657a8d0fce59bcbaf45b
Mehmet Ergün <mrmehmetergun@gmail.com>
2023-02-23 11:31:38 -0500

    Initial Commit

---
+++ RestartApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by Mehmet Ergün on 2023-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Mehmet Ergün on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
